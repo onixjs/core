8484f2713f364ce08e966b0b77ebdd777b761374
Jonathan <jcasarrubias@mean.expert>
2018-05-01 18:32:09 -0500

    Release 1.0.0-alpha.22.2 :ant:
    - Exported missing ACL features

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,3 +5,5 @@ export * from './host.boot';
 export * from './app.server';
 export * from './app.factory';
 export * from './app.notifier';
+export * from './acl.groups';
+export * from './acl.everyone';
--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ export class OnixJS {
    * @description Current Onix Version.
    */
   get version(): string {
-    return '1.0.0-alpha.22.1';
+    return '1.0.0-alpha.22.2';
   }
   /**
    * @property router
