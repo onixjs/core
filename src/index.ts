19f3150fbd7dd36011e34876064bf6959360f542
Jonathan <jcasarrubias@mean.expert>
2018-07-21 10:09:19 -0500

    Release 1.0.0-beta.5.2 :ant:
    - Fixed https://github.com/onixjs/core/issues/63

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ export class OnixJS {
    * @description Current Onix Version.
    */
   get version(): string {
-    return '1.0.0-beta.5.1';
+    return '1.0.0-beta.5.2';
   }
   /**
    * @property router
